4bb7ec173bccf6a57faa4852caa30899af8d7717
Anders Nyvang <nanders@nanders.com>
2018-12-11 17:00:35 +0100

    Improved error handling in DoRequest

--- src/CircleCI.js
+++ src/CircleCI.js
@@ -1,18 +1,17 @@
 let token = localStorage.getItem('circle_ci_token');
-if (token == null) {
-  console.log('circle_ci_token is missing from local storage');
-}
 
-let circleci_token = "?circle-token=" + token;
 let circleci_url = "https://circleci.com/api/v1.1/";
+let circleci_token = "?circle-token=" + token;
 
 function DoRequest(url) {
+  if (token == null) {
+    throw new Error('circle_ci_token is missing from local storage');
+  }
+
   return fetch(circleci_url + url + circleci_token, {
     method: 'get'
   }).then((r) => {
     return r.json()
-  }).catch((e) => {
-    console.log(e);
   });
 }
 
