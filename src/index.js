bceeca847d4f12172947a13788af22db39fd818c
Jan Krag <jankrag@gmail.com>
2018-12-11 11:06:21 +0100

    Add ErrorBoundary to handle errors

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
+import ErrorBoundary from './ErrorBoundary';
 import Dashboard from './Dashboard';
 import Tile from './Tile';
 import * as serviceWorker from './serviceWorker';
 
 
 //ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<Dashboard />, document.querySelector("#dashboard"))
+ReactDOM.render(<ErrorBoundary><Dashboard /></ErrorBoundary>, document.querySelector("#dashboard"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
