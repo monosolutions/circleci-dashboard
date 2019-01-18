00af34e70cacfd9f1a440e78043f3aec23b0f025
Anders Nyvang <nanders@nanders.com>
2019-01-18 16:33:36 +0100

    Deployed first version

---
+++ precache-manifest.801fa055d7058365dfc41539acdb57ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a1530c90b11b37dd309e",
+    "url": "/static/js/main.a1530c90.chunk.js"
+  },
+  {
+    "revision": "ef1823a9f035f117e513",
+    "url": "/static/js/1.ef1823a9.chunk.js"
+  },
+  {
+    "revision": "a1530c90b11b37dd309e",
+    "url": "/static/css/main.572e7119.chunk.css"
+  },
+  {
+    "revision": "07c212f821fc19d324e7c9af11eb7a5d",
+    "url": "/index.html"
+  }
+];
