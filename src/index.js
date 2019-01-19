961a6b10878f0f94e154c7310747ec2ce0df6698
Anders Nyvang <nanders@nanders.com>
2019-01-19 09:20:53 +0100

    Switched to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import './index.css';
 import Dashboard from './Dashboard';
 import Config from './Config';
@@ -8,8 +8,8 @@ import Config from './Config';
 ReactDOM.render(
   <Router>
     <div>
-      <Route path={`${process.env.PUBLIC_URL}/`} exact component={Dashboard} />
-      <Route path={`${process.env.PUBLIC_URL}/config`} component={Config} />
+      <Route path="/" exact component={Dashboard} />
+      <Route path="/config" component={Config} />
     </div>
   </Router>,
   document.querySelector("#dashboard"))
