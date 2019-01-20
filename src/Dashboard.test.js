7828dfb077654f02737b7d6cef14bb85fd3a1ce2
Anders Nyvang <nanders@nanders.com>
2019-01-20 12:01:52 +0100

    Fixed test problem - missing routing

--- src/Dashboard.test.js
+++ src/Dashboard.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { HashRouter as Router, Route } from "react-router-dom";
 import ReactDOM from 'react-dom';
 import Dashboard from './Dashboard';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Dashboard />, div);
+  ReactDOM.render(<Router><Route path="/" exact component={Dashboard} /></Router>, div);
   ReactDOM.unmountComponentAtNode(div);
 });
