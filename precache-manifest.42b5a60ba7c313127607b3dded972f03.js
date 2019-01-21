2ddca419443cbf2855eb8d9189dc1c7cd5d2efdf
Anders Nyvang <nanders@nanders.com>
2019-01-21 17:17:50 +0100

    Styling cleanup + backgrounds

---
+++ precache-manifest.42b5a60ba7c313127607b3dded972f03.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "36b8c09249759566695a",
+    "url": "/circleci-dashboard/static/js/runtime~main.36b8c092.js"
+  },
+  {
+    "revision": "755c8f5e78830156b00f",
+    "url": "/circleci-dashboard/static/js/main.755c8f5e.chunk.js"
+  },
+  {
+    "revision": "24c91b032ace4d84afdc",
+    "url": "/circleci-dashboard/static/js/1.24c91b03.chunk.js"
+  },
+  {
+    "revision": "755c8f5e78830156b00f",
+    "url": "/circleci-dashboard/static/css/main.38d32fe9.chunk.css"
+  },
+  {
+    "revision": "9ac7d13282be1237a0c4cae99171bf44",
+    "url": "/circleci-dashboard/index.html"
+  }
+];
